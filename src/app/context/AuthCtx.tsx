249d12a5c5504aa568f7a6791f826f49c0c7e0bb
gabriel medeiros <gabrielmed00@hotmail.com>
2023-11-30 14:00:56 -0300

    add context authetication

---
+++ src/app/context/AuthCtx.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { auth } from "./firebase";
+
+interface AuthCtxType {
+  user: User | null | false;
+}
+
+export interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthCtx = createContext({} as AuthCtxType);
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null | false>(null);
+
+  useEffect(() => {
+    return onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(false);
+      }
+    });
+  }, []);
+
+  const value = useMemo(
+    () => ({
+      user,
+    }),
+    [user]
+  );
+
+  return <AuthCtx.Provider value={value}>{children}</AuthCtx.Provider>;
+};
+
+export const useSession = () => {
+  const { user } = useContext(AuthCtx);
+
+  const isLoading = user === null;
+
+  return { user, isLoading };
+};
